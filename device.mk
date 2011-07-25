b746f513dea07024bf3db23d7ed53e04a47a6a2c
Dan Murphy <dmurphy@ti.com>
2011-07-25 14:22:57 -0500

    [Tablet] Update the device.mk file to the latest
    Update the device.mk to the latest code and fix some errors
    referencing blaze and not blaze_tablet
    
    Change-Id: I80a0d41a3738e809b7076e7e7a15d429bd3a54c5
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- device.mk
+++ device.mk
@@ -33,31 +33,40 @@ else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+#Need to revisit the fastboot copy files
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel \
-        device/ti/blaze/boot/fastboot.sh:fastboot.sh \
-	device/ti/blaze/boot/fastboot:fastboot \
-        $(LOCAL_KERNEL):boot/zImage \
-        device/ti/blaze/boot/MLO_es2.2_emu:boot/MLO_es2.2_emu \
-        device/ti/blaze/boot/MLO_es2.2_gp:boot/MLO_es2.2_gp \
-        device/ti/blaze/boot/u-boot.bin:boot/u-boot.bin \
-	device/ti/blaze/init.omap4blazeboard.rc:root/init.omap4blazeboard.rc \
-	device/ti/blaze/init.omap4blazeboard.usb.rc:root/init.omap4blazeboard.usb.rc \
-	device/ti/blaze/ueventd.omap4blazeboard.rc:root/ueventd.omap4blazeboard.rc \
-	device/ti/blaze/media_profiles.xml:system/etc/media_profiles.xml \
+	device/ti/blaze_tablet/init.omap4blazeboard.rc:root/init.omap4blazeboard.rc \
+	device/ti/blaze_tablet/init.omap4blazeboard.usb.rc:root/init.omap4blazeboard.usb.rc \
+	device/ti/blaze_tablet/ueventd.omap4blazeboard.rc:root/ueventd.omap4blazeboard.rc \
+	device/ti/blaze_tablet/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-device/ti/blaze_tablet/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl \
-	 device/ti/blaze_tablet/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc
+	device/ti/blaze_tablet/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl \
+	device/ti/blaze_tablet/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc \
+        device/ti/blaze_tablet/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+	hardware/ti/omap4/domx/Ducati_binary/etc/firmware/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin
 
 PRODUCT_PACKAGES += \
 	lights.blaze_tablet
 
-PRODUCT_PACKAGES += \
-	sensors.blaze_tablet
+#Remove this as it freezes at boot. Will re-enable once fixed
+#PRODUCT_PACKAGES += \
+#	sensors.blaze_tablet
 
 PRODUCT_PACKAGES += \
 	com.android.future.usb.accessory
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        MagicSmokeWallpapers \
+        VisualizationWallpapers \
+        librs_jni
+
+PRODUCT_PACKAGES += \
+	VideoEditorGoogle
 
 PRODUCT_PROPERTY_OVERRIDES := \
 	hwui.render_dirty_regions=false
@@ -67,6 +76,14 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+PRODUCT_CHARACTERISTICS := tablet
+
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
@@ -75,10 +92,30 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-	make_ext4fs
+	make_ext4fs \
+	setup_fs
 
 # Audio HAL module
-PRODUCT_PACKAGES += audio.primary.blaze
+#PRODUCT_PACKAGES += audio.primary.blaze_tablet \
+#	audio.a2dp.default
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
 
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
+$(call inherit-product, hardware/ti/omap4/omap4.mk)
 $(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
