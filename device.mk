33edfa4d52e2e07461033106beaaa3c966151ab0
x0118064 <saravanan.s@ti.com>
2011-10-24 16:15:11 -0500

    BlazeTablet: Add SkLibTiJpeg_Test to the product list.
    Change-Id: I58c2cd8892927ef2e6ae2408e96be78deba2e4b1
    Signed-off-by: Solaiyappan Saravanan <saravanan.s@ti.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     hwcomposer.omap4
 
+#Lib Skia test
+PRODUCT_PACKAGES += \
+    SkLibTiJpeg_Test
+
 # Camera
 PRODUCT_PACKAGES += \
     CameraOMAP4 \
