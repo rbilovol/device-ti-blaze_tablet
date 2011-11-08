7995f8f740486a73b90d5c5be34be88ce1150c27
Dan Murphy <dmurphy@ti.com>
2011-11-08 07:10:49 -0600

    [Tablet] Enable building of recovery image
    Add definitions for the recovery image.
    
    Change-Id: I06a743b1a9e49a415854c7ca7d4f320e0e4bbe04
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,6 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 BOARD_HAVE_BLUETOOTH := true
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_RECOVERY := true
 
 BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE
@@ -45,6 +44,13 @@ BOARD_EGL_CFG := device/ti/blaze_tablet/egl.cfg
 #BOARD_USES_HGL := true
 #BOARD_USES_OVERLAY := true
 USE_OPENGL_RENDERER := true
+
+# Recovery
+#TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_UI_LIB := librecovery_ui_blaze_tablet
+# device-specific extensions to the updater binary
+TARGET_RELEASETOOLS_EXTENSIONS := device/ti/blaze_tablet
+
 BOARD_USES_SECURE_SERVICES := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,11 +1,11 @@
-ifeq ($(TARGET_DEVICE),blaze)
+ifeq ($(TARGET_DEVICE),blaze_tablet)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES += bootable/recovery
-LOCAL_SRC_FILES := recovery_ui.c
+LOCAL_SRC_FILES := recovery_ui.cpp
 
 # should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
 LOCAL_MODULE := librecovery_ui_blaze_tablet
