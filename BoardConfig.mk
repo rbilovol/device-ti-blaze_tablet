c7b3196cc8e6aa668d04c536c45cc10cf627ab28
Jean Johnson <jean-johnson@ti.com>
2011-11-16 15:01:58 -0600

    Disabling recovery system due to missing dependencies
    Disabling the new recovery changes due to missing
    dependencies.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 BOARD_HAVE_BLUETOOTH := true
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_RECOVERY := true
 
 BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE
@@ -47,9 +48,9 @@ USE_OPENGL_RENDERER := true
 
 # Recovery
 #TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TARGET_RECOVERY_UI_LIB := librecovery_ui_blaze_tablet
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_blaze_tablet
 # device-specific extensions to the updater binary
-TARGET_RELEASETOOLS_EXTENSIONS := device/ti/blaze_tablet
+#TARGET_RELEASETOOLS_EXTENSIONS := device/ti/blaze_tablet
 
 BOARD_USES_SECURE_SERVICES := true
 
