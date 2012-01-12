82a970e534d76530d663eb2dbc7245d633c59c13
Viswanath Puttagunta <vishp@ti.com>
2012-01-12 13:15:01 -0600

    blaze_tablet: Remove permissions for nfc
    nfc is not supported in Blaze Tablet. So,
    remove permissions for it. This fixes CTS
    test failure for
    android.app.cts.SystemFeaturesTest#testNfcFeatures
    
    Change-Id: I08e42fe527b9b25b30d58d51b32535d0b59974d6
    Signed-off-by: Viswanath Puttagunta <vishp@ti.com>

--- device.mk
+++ device.mk
@@ -154,7 +154,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
 	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
