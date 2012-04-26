4bb04d1e7a4af3c2942372ddb5ed16a243358708
Peter Nordström <nordstrom@ti.com>
2012-04-25 17:15:36 -0700

    blaze_tablet: Add Cypress touch screen config file
    Add the cyttsp4-i2c.idc file to identify the
    cypress touch screen to the Android FS
    
    Change-Id: I6f657aa6ce452c783b06f2f37be154b88228c8bb
    Signed-off-by: Peter Nordström <nordstrom@ti.com>

--- device.mk
+++ device.mk
@@ -63,7 +63,8 @@ PRODUCT_COPY_FILES := \
 	device/ti/blaze_tablet/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl \
 	device/ti/blaze_tablet/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc \
         device/ti/blaze_tablet/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-	device/ti/blaze_tablet/bootanimation.zip:/system/media/bootanimation.zip
+	device/ti/blaze_tablet/bootanimation.zip:/system/media/bootanimation.zip \
+	device/ti/blaze_tablet/cyttsp4-i2c.idc:system/usr/idc/cyttsp4-i2c.idc
 
 # to mount the external storage (sdcard)
 PRODUCT_COPY_FILES += \
