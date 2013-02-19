72c3cf3d70f1f803faa7523cb66abe6163073602
Andrii Beregovenko <a.beregovenko@ti.com>
2013-02-19 20:14:25 +0200

    blaze_tablet: use tablet core hardware instead of handheld
    Change-Id: Id90427e75fba61a1e484c3d1c899b038723ca69c
    Signed-off-by: Andrii Beregovenko <a.beregovenko@ti.com>

--- device.mk
+++ device.mk
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
